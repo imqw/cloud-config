527da9ee86bde3b3fb0eacd9fdd4e38fa6ba6f1d
qiuwei <Wcbc0310@126.com>
2019-07-07 19:35:43 +0800

    初始化项目

---
+++ config-client/src/main/java/com/config/client/ClientController.java
@@ -0,0 +1,27 @@
+package com.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: qiuwei@19pay.com.cn
+ * @Version 1.0.0
+ */
+@RestController
+public class ClientController {
+
+    @Value("${profile}")
+    private String profile;
+
+    @Value("${name}")
+    private String name;
+
+
+    @GetMapping("/profile")
+    public String hello(){
+
+        return this.profile +"   :    "+this.name;
+    }
+
+}
